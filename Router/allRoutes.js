760ef98c975b922f18895b0fa6ffff4c201adb0b
anaisdev2024 <anaisibari@gmail.com>
2026-04-19 16:14:16 +0200

    ajout form reservation + page la carte

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,11 +4,13 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "./pages/home.html"),
     new Route("/galerie", "La galerie", "./pages/galerie.html"),
+    new Route("/lacarte", "La carte", "./pages/la carte.html"),
     new Route("/signin","Connexion","./pages/auth/signin.html"),
     new Route("/signup","Inscription","./pages/auth/signup.html"),
     new Route("/account","Mon compte","./pages/auth/account.html"),
     new Route("/editPassword","Changement de mot de passe","./pages/auth/editPassword.html"),
     new Route("/allresa","Vos Réservations","./pages/reservations/allresa.html"),
+    new Route("/reserver","Réserver","./pages/reservations/reserver.html"),
 ];  
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
