5cbc350803829abf5d39fedd38a60655c2ab3ea1
anaisdev2024 <anaisibari@gmail.com>
2026-04-23 18:38:34 +0200

    ajout verification champs nom prenom email en JS

---
+++ js/auth/signup.js
@@ -0,0 +1,57 @@
+//Implémenter le JS de ma page
+
+const inputNom = document.getElementById("NomInput");
+const inputPreNom = document.getElementById("PrenomInput");
+const inputMail = document.getElementById("EmailInput");
+const inputPassword = document.getElementById("PasswordInput");
+const inputValidationPassword = document.getElementById("ValidatePasswordInput");
+const btnValidation = document.getElementById("btn-validation-inscription");
+
+inputNom.addEventListener("keyup", validateForm); 
+inputPreNom.addEventListener("keyup", validateForm);
+inputMail.addEventListener("keyup", validateForm);
+inputPassword.addEventListener("keyup", validateForm);
+inputValidationPassword.addEventListener("keyup", validateForm);
+
+//Function permettant de valider tout le formulaire
+function validateForm(){
+    const nomOK=validateRequired(inputNom);
+    const prenomOK=validateRequired(inputPreNom);
+    const mailOK=validateMail(inputMail);
+  
+
+    if(nomOK && prenomOK && mailOK){
+        btnValidation.disabled=false;
+   }
+    else{ 
+        btnValidation.disabled=true;
+    }
+
+function validateRequired(input){
+    if(input.value != ''){
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid"); 
+        return true;
+    }
+    else{
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+        return false;
+    }
+}
+
+function validateMail(input){
+    //Définir mon regex
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const mailUser = input.value;
+    if(mailUser.match(emailRegex)){
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid"); 
+        return true;
+    }
+    else{
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+        return false;
+    }
+} }
